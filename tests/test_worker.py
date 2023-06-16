26f7438a42786dfb8a8b77c81a352722b6f253a1
xrsrke <xariusdrake@hotmail.com>
2023-06-16 15:16:44 +0700

    add tests

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -9,6 +9,7 @@ from pipegoose.worker import spawn_worker
 def test_spawn_worker():
     DEVICES = [torch.device("cpu"), torch.device("cpu")]
     TARGET_DEVICE_IDX = len(DEVICES) - 1
+    N_LOOP = 10
 
     count = 0
     lock = threading.Lock()
@@ -20,9 +21,13 @@ def test_spawn_worker():
             return 1
 
     with spawn_worker(DEVICES) as (in_queues, out_queues):
-        for _ in range(10):
+        for _ in range(N_LOOP):
             time.sleep(0.7)
             in_queues[TARGET_DEVICE_IDX].put(counter)
 
     # TODO: check why 9 not 10?
     assert count == 9
+
+    for _ in range(N_LOOP):
+        output = out_queues[TARGET_DEVICE_IDX].get()
+        assert output == (1, True)
