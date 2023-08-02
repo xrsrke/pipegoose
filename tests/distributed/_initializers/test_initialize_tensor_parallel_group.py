60094119b62f4e8a16597e3055f0a9cba664f224
xrsrke <b3f0cus@icloud.com>
2023-08-02 11:23:43 +0700

    refactor

--- tests/distributed/_initializers/test_initialize_tensor_parallel_group.py
+++ tests/distributed/_initializers/test_initialize_tensor_parallel_group.py
@@ -1,5 +1,6 @@
 import pytest
 import torch
+import torch.distributed as dist
 from torch.distributed import ProcessGroup
 
 from pipegoose.distributed._initializers.initialize_tensor import (
@@ -33,8 +34,17 @@ def init_tensor_parallel_group(rank, world_size, host, port, tensor_parallel_siz
     assert isinstance(result["ranks_in_group"], list)
     assert result["parallel_mode"] == ParallelMode.TENSOR
 
+    dist.destroy_process_group(result["process_group"])
+    dist.destroy_process_group()
 
-@pytest.mark.parametrize("world_size, tensor_parallel_size, pipeline_parallel_size, data_parallel_size", [(1, 1, 1, 1)])
+
+@pytest.mark.parametrize(
+    "world_size, tensor_parallel_size, pipeline_parallel_size, data_parallel_size",
+    [
+        (1, 1, 1, 1),
+        # (8, 2, 2, 2)
+    ],
+)
 def test_init_tensor_parallel_group(world_size, tensor_parallel_size, pipeline_parallel_size, data_parallel_size):
     PORT = 12345
 
