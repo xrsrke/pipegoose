c22569720cf3bcced650beec08aeca87a58e3a8c
xrsrke <b3f0cus@icloud.com>
2023-08-14 13:12:39 +0700

    add parallel mapping

---
+++ pipegoose/nn/tensor_parallel/parallel_mapping.py
@@ -0,0 +1,50 @@
+from typing import Tuple
+
+
+class TensorParallelInformation:
+    def __init__(self, module_name: Tuple[str]):
+        self.module_name = module_name
+
+
+class Column(TensorParallelInformation):
+    pass
+
+
+class Row(TensorParallelInformation):
+    pass
+
+
+class ParallelMapping:
+    # TODO: make this extendable
+    # so user can define their own mapping
+
+    __MAPPING__ = {
+        "bloom-560m": [
+            Column(("dense_h_to_4h",)),
+            Row(("dense_4h_to_h",)),
+        ]
+    }
+
+    def _search(cls, module_name: str) -> TensorParallelInformation:
+        """
+        Search for module_name in mappings.
+        """
+        for _, items in cls.__MAPPING__.items():
+            for item in items:
+                if module_name in item.module_name:
+                    return item
+        return None
+
+    @classmethod
+    def is_column_parallel(cls, module_name: str) -> bool:
+        item = cls._search(module_name)
+        if item is None:
+            return False
+        return isinstance(item, Column)
+
+    @classmethod
+    def is_row_parallel(cls, module_name: str) -> bool:
+        item = cls._search(module_name)
+        if item is None:
+            return False
+        return isinstance(item, Row)
