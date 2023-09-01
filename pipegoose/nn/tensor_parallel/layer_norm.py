c163ecdd94a0ad8827454365ea1f1f912e77e689
xrsrke <b3f0cus@icloud.com>
2023-09-01 11:17:22 +0700

    refactor

--- pipegoose/nn/tensor_parallel/layer_norm.py
+++ pipegoose/nn/tensor_parallel/layer_norm.py
@@ -8,6 +8,8 @@ from pipegoose.distributed.parallel_context import ParallelContext
 class LayerNorm(nn.Module):
     def __init__(self, normalized_shape: int, eps: float = 1e-5, bias: bool = True, parallel_context: ParallelContext = None):
         super().__init__()
+        assert parallel_context is not None, "parallel_context must be provided"
+
         self.normalized_shape = normalized_shape
         self.eps = eps
         self.parallel_context = parallel_context
@@ -19,9 +21,5 @@ class LayerNorm(nn.Module):
             self.register_parameter("bias", None)
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        if isinstance(self.normalized_shape, int):
-            normalized_shape = (self.normalized_shape,)
-        else:
-            normalized_shape = self.normalized_shape
-
+        normalized_shape = self.normalized_shape if isinstance(self.normalized_shape, int) else self.normalized_shape
         return F.layer_norm(input, normalized_shape, self.weight, self.bias, self.eps)
