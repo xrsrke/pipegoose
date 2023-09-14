841d7474de08f8d98319d402fad402762da49d22
xrsrke <b3f0cus@icloud.com>
2023-09-14 12:12:33 +0700

    add naive model partitioner for pipeline parallelism

---
+++ pipegoose/nn/pipeline_parallel2/partitioner.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractclassmethod
+from typing import List
+
+from torch import nn
+
+
+from pipegoose.distributed.parallel_context import ParallelContext
+
+
+class BasePartitioner(ABC):
+    @abstractclassmethod
+    def split(self):
+        raise NotImplementedError
+
+
+class NaivePartitioner(BasePartitioner):
+    def __init__(self, module: nn.Module, parallel_context: ParallelContext):
+        self.module = module
+        self.parallel_context = parallel_context
+
+    def split(self) -> List[nn.Module]:
+        module = self.module
+        n_partitions = self.parallel_context.pipeline_parallel_size
+
+        embedding_module = module.transformer.word_embeddings
+        transformer_blocks = module.transformer.h
+        lm_head = module.lm_head
+
+        # Calculate the number of transformer blocks per partition
+        blocks_per_partition = len(transformer_blocks) // n_partitions
+        partitions = []
+
+        # for i in range(n_partitions - 2):
+        for i in range(n_partitions):
+            start = i * blocks_per_partition
+            # if it's the last partition, get all remaining blocks
+            end = start + blocks_per_partition if i < n_partitions - 1 else None
+            partitions.append(nn.Sequential(*transformer_blocks[start:end]))
+
+        partitions[0] = nn.Sequential(embedding_module, partitions[0])
+        partitions[-1] = nn.Sequential(lm_head, partitions[-1])
+
+        return partitions
---
+++ tests/nn/pipeline_parallel_2/test_partitioner.py
@@ -0,0 +1,59 @@
+import pytest
+
+from transformers import AutoModelForCausalLM
+
+from pipegoose.distributed.parallel_context import ParallelContext
+from pipegoose.nn.pipeline_parallel2.partitioner import NaivePartitioner
+from pipegoose.testing.utils import spawn
+
+
+MODEL_NAME = "bigscience/bloom-560m"
+
+
+@pytest.fixture(scope="session")
+def model():
+    return AutoModelForCausalLM.from_pretrained(MODEL_NAME)
+
+
+def init_parallel_context(rank, world_size, port, tensor_parallel_size, pipeline_parallel_size, data_parallel_size):
+    parallel_context = ParallelContext(
+        rank=rank,
+        local_rank=rank,
+        world_size=world_size,
+        local_world_size=world_size,
+        host="localhost",
+        port=port,
+        seed=69,
+        backend="gloo",
+        tensor_parallel_size=tensor_parallel_size,
+        pipeline_parallel_size=pipeline_parallel_size,
+        data_parallel_size=data_parallel_size,
+    )
+
+    return parallel_context
+
+
+def run_naive_partitioning(rank, world_size, port, tensor_parallel_size, pipeline_parallel_size, data_parallel_size, module):
+    parallel_context = init_parallel_context(
+        rank, world_size, port, tensor_parallel_size, pipeline_parallel_size, data_parallel_size
+    )
+
+    partitioner = NaivePartitioner(module, parallel_context)
+    partitions = partitioner.split()
+
+    assert len(partitions) == pipeline_parallel_size
+
+
+@pytest.mark.parametrize("pipeline_parallel_size", [1, 2, 5])
+def test_naive_partitioning(model, pipeline_parallel_size):
+    TENSOR_PARALLEL_SIZE = 1
+    DATA_PARALLEL_SIZE = 1
+
+    spawn(
+        run_naive_partitioning,
+        world_size=pipeline_parallel_size,
+        tensor_parallel_size=TENSOR_PARALLEL_SIZE,
+        pipeline_parallel_size=pipeline_parallel_size,
+        data_parallel_size=DATA_PARALLEL_SIZE,
+        module=model
+    )
