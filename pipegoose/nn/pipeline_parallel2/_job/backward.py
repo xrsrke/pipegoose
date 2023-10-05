fixing the gradient flow to leaf nodes in the backward job

pipegoose/nn/pipeline_parallel2/_job/backward.py
@@ -1,26 +1,23 @@
 import torch
 
-from pipegoose.nn.pipeline_parallel2._job.callback import Callback
 from pipegoose.nn.pipeline_parallel2._job.job import Job
-from pipegoose.nn.pipeline_parallel2._package import Package
 from pipegoose.nn.pipeline_parallel2.queue import SavedActivation
 
+# class CreateBackwardOutputPackageCallback(Callback):
+#     """Create a new package for the output of a backward job."""
 
-class CreateBackwardOutputPackageCallback(Callback):
-    """Create a new package for the output of a backward job."""
+#     def after_compute(self):
+#         data = self.job.output
+#         orig_metadata = self.job.input.metadata
 
-    def after_compute(self):
-        data = self.job.output
-        orig_metadata = self.job.input.metadata
+#         package = Package(data, orig_metadata)
+#         package.metadata.partition_idx -= 1
 
-        package = Package(data, orig_metadata)
-        package.metadata.partition_idx -= 1
-
-        self.job.output = package
+#         self.job.output = package
 
 
-class SendBackwardPackageCallback(Callback):
-    pass
+# class SendBackwardPackageCallback(Callback):
+#     pass
 
 
 class BackwardJob(Job):
@@ -37,7 +34,10 @@ class BackwardJob(Job):
 
         print(f"executing backward job, rank={rank}, microbatch_idx={microbatch_idx}, partition_idx={partition_idx}")
 
-        with torch.enable_grad():
-            torch.autograd.backward(output, prev_grad)
+        # with torch.enable_grad():
+        torch.autograd.backward(output, grad_tensors=prev_grad)
 
-        return output.grad
+        # TODO: remove this, since the grads is stored in module's weights
+        # and we do gradient accumulation, we don't need return grads or send to other stages
+
+        return torch.randn_like(output)

pipegoose/nn/pipeline_parallel2/_job/creator.py
@@ -2,16 +2,13 @@ from abc import ABC, abstractmethod
 from typing import Any, Callable, Union
 
 import torch
-import torch.distributed.rpc as rpc
 
 from pipegoose.nn.pipeline_parallel2._comm import (
     get_pipeline_context,
     set_pipeline_context,
 )
-from pipegoose.nn.pipeline_parallel2._job.backward import (
+from pipegoose.nn.pipeline_parallel2._job.backward import (  # CreateBackwardOutputPackageCallback,; SendBackwardPackageCallback,
     BackwardJob,
-    CreateBackwardOutputPackageCallback,
-    SendBackwardPackageCallback,
 )
 from pipegoose.nn.pipeline_parallel2._job.forward import (
     ConfirmCompleteATaskToProgressTracker,
@@ -52,11 +49,11 @@ class _ForwardJobCreator(JobCreator):
 
 
 class _BackwardJobCreator(JobCreator):
-    CBS = [CreateBackwardOutputPackageCallback, SendBackwardPackageCallback]
+    # CBS = [CreateBackwardOutputPackageCallback, SendBackwardPackageCallback]
 
     @classmethod
     def create(cls, function: Callable, package: Package, pipeline_context: PipelineContext) -> BackwardJob:
-        job = BackwardJob(function, package, cbs=cls.CBS, pipeline_context=pipeline_context)
+        job = BackwardJob(function, package, pipeline_context=pipeline_context)
         return job
 
 
@@ -128,25 +125,26 @@ def schedule_backward_job(package: Package, pipeline_context: PipelineContext) -
             microbatch_idx = metadata.microbatch_idx
 
             dst_worker_name = parallel_context.get_worker_name(metadata.dst)
+            print(grad_input)
             print(f"creating a backward job, rank={rank}, microbatch_idx={microbatch_idx}, dst_worker_name={dst_worker_name}")
 
+            _create_backward_job_and_put_to_pending_queue(grad_input, metadata)
             # TODO: because forward job and backward job are in the same node
             # rpc isn't necessary
-            rpc.rpc_sync(
-                # NOTE: the backward job create in the same node
-                # as the forward job
-                to=dst_worker_name,
-                func=_create_backward_job_and_put_to_pending_queue,
-                args=(grad_input, metadata),
-            )
+            # rpc.rpc_sync(
+            #     # NOTE: the backward job create in the same node
+            #     # as the forward job
+            #     to=dst_worker_name,
+            #     func=_create_backward_job_and_put_to_pending_queue,
+            #     args=(grad_input, metadata),
+            # )
 
             return (None, None, None)
 
     set_pipeline_context(pipeline_context)
 
-    data = package.data
     metadata = package.metadata
-    data = _ScheduleBackwardJob.apply(metadata, pipeline_context, data)
+    data = _ScheduleBackwardJob.apply(metadata, pipeline_context, package.data)
     package.data = data
 
     return package

tests/nn/pipeline_parallel_2/job/test_backward.py
@@ -69,6 +69,8 @@ def run_create_a_backward_job_if_a_tensor_do_backprop(
         backward_job = JobQueue.PENDING_JOBS.get()
         assert isinstance(backward_job, BackwardJob)
 
+        # backward_job.compute()
+
     # NOTE: wait for the backward job to be created
     dist.barrier()
     time.sleep(0.1)
@@ -96,28 +98,35 @@ def test_create_a_backward_job_if_a_tensor_do_backprop_in_the_same_node(request,
 
 
 def test_execute_a_backward_job(backward_job):
+    BATCH_SIZE = 2
+    SEQ_LEN = 5
+    HIDDEN_SIZE = 10
+    linear = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE)
+
+    input = torch.randn(BATCH_SIZE, SEQ_LEN, HIDDEN_SIZE, requires_grad=True)
+    INPUT = deepcopy(input)
+    LINEAR = deepcopy(linear)
+    OUTPUT = LINEAR(INPUT)
+    INITIAL_GRADS = torch.ones_like(OUTPUT)
+
+    OUTPUT.sum().backward()
+
     MICROBATCH_IDX = backward_job.input.metadata.microbatch_idx
     PARTITION_IDX = backward_job.input.metadata.partition_idx
 
-    input = torch.randn_like(backward_job.input.data)
-    linear = nn.Linear(input.shape[1], input.shape[0])
-    output = linear(input).sum()
-    # NOTE: set initial gradients for the backward job
-    backward_job.input.data = torch.ones_like(output)
+    backward_job.input.data = INITIAL_GRADS
 
-    output.backward(retain_graph=True)
-    GROUND_W_GRAD = deepcopy(linear.weight.grad)
-    GROUND_B_GRAD = deepcopy(linear.bias.grad)
-    linear.weight.grad.zero_()
-    linear.bias.grad.zero_()
-
+    # NOTE: stores the output activations that the backward job
+    # will use to compute the gradients
     key = SavedActivation.get_key(MICROBATCH_IDX, PARTITION_IDX)
+    output = linear(input)
     SavedActivation.save_activations(key, output)
 
     _ = backward_job.compute()
 
-    assert torch.equal(linear.weight.grad, GROUND_W_GRAD)
-    assert torch.equal(linear.bias.grad, GROUND_B_GRAD)
+    assert torch.equal(input.grad, INPUT.grad)
+    assert torch.equal(linear.weight.grad, LINEAR.weight.grad)
+    assert torch.equal(linear.bias.grad, LINEAR.bias.grad)
 
 
 @pytest.mark.skip