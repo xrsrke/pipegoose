32a9416fa012f8b5bb674e8d2fa94217a6b66c77
xrsrke <b3f0cus@icloud.com>
2023-10-09 10:15:36 +0700

    wip zero

--- pipegoose/optim/zero/optim.py
+++ pipegoose/optim/zero/optim.py
@@ -1,47 +1,82 @@
-from typing import Any, Dict, Iterable, List
-
-import torch
-from torch import nn
 from torch.optim import Optimizer
 
+from pipegoose.distributed.functional import broadcast
 from pipegoose.distributed.parallel_context import ParallelContext
-from pipegoose.optim import DistributedOptimizer
+from pipegoose.distributed.parallel_mode import ParallelMode
+from pipegoose.optim import BaseDistributedOptimizer
 from pipegoose.optim.zero.sharding import ParameterSharding
 
 
-class ZeroRedundancyOptimizer(DistributedOptimizer):
+class DistributedOptimizer(BaseDistributedOptimizer):
+    """ZeRO-1 optimizer that works natively in 3D parallelism."""
+
     def __init__(
         self,
-        params: Iterable[List[nn.Parameter]],
         optim: Optimizer,
         parallel_context: ParallelContext,
-        sharding: ParameterSharding,
-        **default: Any
     ):
-        # TODO: accept params, and optim separately
-        self.params = params
-        self._optim_constructor = optim
+        self.optim = optim
         self.parallel_context = parallel_context
-        self.default = default
 
-        # world_size = parallel_context.get_world_size(ParallelMode.GLOBAL)
+        self._master_params = None
 
-    def _partition_and_move_to_rank(self):
-        self._partition_params(self.optim.param_groups)
+        self._setup_local_optim()
 
-    def _construct_local_optim(self, local_params: Dict[str, torch.Tensor]):
-        self.optim = self._optim_constructor(local_params, **self.default)
+    # def _sync_hyperparams(self, source: List[Dict[Any, Any]], destination: List[Dict[Any, Any]]):
+    #     for source_group, destination_group in zip(source, destination):
+    #         for k in source_group.keys():
+    #             if k != "params":
+    #                 destination_group[k] = source_group[k]
 
-    def _sync_hyperparams(self, source: List[Dict[Any, Any]], destination: List[Dict[Any, Any]]):
-        for source_group, destination_group in zip(source, destination):
-            for k in source_group.keys():
-                if k != "params":
-                    destination_group[k] = source_group[k]
+    def _setup_local_optim(self):
+        """Setup local optimizer."""
+        local_rank = self.parallel_context.get_local_rank(ParallelMode.DATA)
 
-    def setup(self):
-        pass
+        # optim = self._optim_constructor(self.params, **self.default)
+        # NOTE: shard and assign the corresponding local parameters to the local optimizer
+        sharded_param_groups = ParameterSharding(self.optim.param_groups, self.parallel_context, ParallelMode.DATA).shard()
+        self._master_params = {rank: params for rank, params in enumerate(sharded_param_groups)}
+        self.optim.param_groups = sharded_param_groups[local_rank]
 
-    def step(self, **kwargs):
-        self._sync_hyperparams(self.param_groups, self.optim.param_groups)
+    # def _construct_local_optim(self, local_params: Dict[str, torch.Tensor]) -> Optimizer:
+    #     optim = self._optim_constructor(local_params, **self.default)
+    #     return optim
 
-        self.optim.step(**kwargs)
+    @property
+    def defaults(self):
+        """Return the default hyperparameters."""
+        return self.optim.defaults
+
+    @property
+    def param_groups(self):
+        """Return the parameter groups."""
+        return self.optim.param_groups
+
+    def add_param_group(self, *args, **kwargs):
+        """Add a new parameter group to the optimizer."""
+        self.optim.add_param_group(*args, **kwargs)
+
+    def load_state_dict(self, *args, **kwargs):
+        """Load the optimizer state."""
+        self.optim.load_state_dict(*args, **kwargs)
+
+    def state_dict(self):
+        """Return the state of the optimizer"""
+        return self.optim.state_dict()
+
+    def step(self, *args, **kwargs):
+        # NOTE: each rank updates its subset of parameters using the local optimizer
+        self.optim.step(*args, **kwargs)
+
+        # NOTE: update the master parameters from the updated local parameters
+
+        # NOTE: each model replicas broadcast the updated parameters to other model replicas
+        rank = self.parallel_context.get_local_rank(ParallelMode.DATA)
+        for group in self.optim.param_groups:
+            for p in group["params"]:
+                if p.requires_grad is True:
+                    broadcast(p, src=rank, parallel_context=self.parallel_context, parallel_mode=ParallelMode.DATA)
+
+    def zero_grad(self, *args, **kwargs):
+        """Zero out gradients."""
+        self.optim.zero_grad(*args, **kwargs)
--- tests/nn/optim/zero/test_optim.py
+++ tests/nn/optim/zero/test_optim.py
@@ -1,14 +1,86 @@
-# from pipegoose.optim import ZeroRedundancyOptimizer
+from copy import deepcopy
+
+import pytest
+import torch
+from torch import nn, optim
+
+from pipegoose.optim.zero import DistributedOptimizer
+from pipegoose.testing.utils import init_parallel_context, spawn
 
 
-# def test_optim():
-#     WORLD_SIZE = 10
+def count_parameters(optimizer):
+    return sum(p.numel() for group in optimizer.param_groups for p in group["params"])
 
-#     params = []
-#     sizes = [2, 3, 4] * WORLD_SIZE
 
-#     for size in sizes:
-#         params.append([torch.randn((size, 1), requires_grad=True)])
+def run_dist_optim(
+    rank,
+    world_size,
+    port,
+    tensor_parallel_size,
+    pipeline_parallel_size,
+    data_parallel_size,
+    input,
+    model,
+    updated_model,
+    optimizer,
+):
+    parallel_context = init_parallel_context(
+        rank, world_size, port, tensor_parallel_size, pipeline_parallel_size, data_parallel_size
+    )
+    ORIG_UPDATED_MODEL = deepcopy(updated_model)
+    ORIG_OPTIM = deepcopy(optimizer)
+    optimizer = optim.Adam(model.parameters())
+    dist_optimizer = DistributedOptimizer(optimizer, parallel_context)
 
-#     optim = SGD(params, lr=0.1)
-#     dist_optim = ZeroRedundancyOptimizer
+    assert dist_optimizer.defaults == optimizer.defaults
+    assert len(dist_optimizer.param_groups) == len(optimizer.param_groups)
+    assert dist_optimizer.state_dict().keys() == optimizer.state_dict().keys()
+    # NOTE: test whether the optimizer partitions the parameters across data parallel dimension
+    assert count_parameters(dist_optimizer) < count_parameters(ORIG_OPTIM)
+
+    dist_optimizer.zero_grad()
+    model(input).sum().backward()
+    dist_optimizer.step()
+
+    # NOTE: test whether the model parameters are updated correctly
+    for p1, p2 in zip(model.parameters(), ORIG_UPDATED_MODEL.parameters()):
+        assert not torch.allclose(p1, p2), f"p1: {p1}, p2: {p2}"
+
+    # dist_optimizer.zero_grad()
+
+    # for p in model.parameters():
+    #     assert p.grad is None
+
+
+@pytest.mark.parametrize("data_parallel_size", [2, 5])
+def test_dist_optim(data_parallel_size):
+    TENSOR_PARALLEL_SIZE = 1
+    PIPELINE_PARALLEL_SIZE = 1
+    WORLD_SIZE = TENSOR_PARALLEL_SIZE * PIPELINE_PARALLEL_SIZE * data_parallel_size
+
+    # LR = 1e-3
+    BATCH_SIZE = 5
+    HIDDEN_SIZE = 10
+    OUTPUT_SIZE = 2
+
+    input = torch.randn(BATCH_SIZE, HIDDEN_SIZE)
+    model = nn.Sequential(nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE), nn.ReLU(), nn.Linear(HIDDEN_SIZE, OUTPUT_SIZE))
+    ORIG_INPUT = deepcopy(input)
+    ORIG_MODEL = deepcopy(model)
+    optimizer = optim.Adam(model.parameters())
+    optimizer.zero_grad()
+
+    model(input).sum().backward()
+    optimizer.step()
+
+    spawn(
+        run_dist_optim,
+        world_size=WORLD_SIZE,
+        tensor_parallel_size=TENSOR_PARALLEL_SIZE,
+        pipeline_parallel_size=PIPELINE_PARALLEL_SIZE,
+        data_parallel_size=data_parallel_size,
+        input=ORIG_INPUT,
+        model=ORIG_MODEL,
+        updated_model=model,
+        optimizer=optimizer,
+    )
