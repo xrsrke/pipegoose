WIP fixed the calculation of the gradients of a single backward job; need to refactor backward jobs and send gradients across pipeline stages

pipegoose/nn/pipeline_parallel2/_job/backward.py
@@ -18,9 +18,10 @@ class BackwardJob(Job):
 
         output = get_output_activations(microbatch_idx, partition_idx)
         input = get_input_activations(microbatch_idx, partition_idx)
+
         torch.autograd.backward(output, grad_tensors=prev_grad)
 
-        if input.grad is None:
+        if input.requires_grad is False:
             raise PipelineGradientFlowError("Gradients can't flow back to the input of the pipeline stage")
 
         # TODO: remove this, since the grads is stored in module's weights
@@ -30,5 +31,8 @@ class BackwardJob(Job):
         rank = self.pipeline_context.parallel_context.get_global_rank()
         print(f"executing backward job, rank={rank}, microbatch_idx={microbatch_idx}, partition_idx={partition_idx}")
         print(f"yay! gradients: {input.grad.shape}")
+
+        if input.grad is None:
+            raise PipelineGradientFlowError("Gradients can't flow back to the input of the pipeline stage")
 
         return input.grad

pipegoose/nn/pipeline_parallel2/pipeline_engine.py
@@ -1,4 +1,3 @@
-import time
 from dataclasses import dataclass
 
 import torch
@@ -13,7 +12,11 @@ from pipegoose.nn.pipeline_parallel2._job.job_type import JobType
 from pipegoose.nn.pipeline_parallel2._package import Metadata, Package, TrainingMetadata
 from pipegoose.nn.pipeline_parallel2._worker import BaseWorkerManager
 from pipegoose.nn.pipeline_parallel2.pipeline_context import PipelineContext
-from pipegoose.nn.pipeline_parallel2.queue import JobQueue, save_input_activations
+from pipegoose.nn.pipeline_parallel2.queue import (
+    JobQueue,
+    get_output_activations,
+    save_input_activations,
+)
 from pipegoose.nn.pipeline_parallel2.scheduler import BaseScheduler
 from pipegoose.nn.pipeline_parallel2.sync.callback import Callback
 from pipegoose.nn.pipeline_parallel2.sync.handshake import (
@@ -142,27 +145,23 @@ class PipelineEngine:
         dist.barrier()
 
         if self.pipeline_context.is_last_stage:
-            from pipegoose.nn.pipeline_parallel2.queue import (
-                _SAVED_ACTIVATIONS,
-                SavedActivation,
-            )
 
             # TODO: use SavedActivation.get_key()
             partition_idx = self.pipeline_context.partition_idx
             outputs = []
-
             for microbatch_idx in range(n_microbatches):
-                key = SavedActivation.get_key(microbatch_idx=microbatch_idx, partition_idx=partition_idx)
-                outputs.append(_SAVED_ACTIVATIONS[key])
+                # key = SavedActivation.get_key(microbatch_idx=microbatch_idx, partition_idx=partition_idx)
+                # outputs.append(_SAVED_ACTIVATIONS[key])
+                outputs.append(get_output_activations(microbatch_idx, partition_idx))
 
             # outputs = [SavedActivation.get_saved_activations((microbatch_idx, partition_idx)) for microbatch_idx in range(n_microbatches)]
-            outputs = torch.cat(outputs, dim=0)
+            # outputs = torch.cat(outputs, dim=0)
             return outputs
-        else:
-            # NOTE: not terminate the worker, make it wait for processing further backward jobs
-            time.sleep(100)
+        # else:
+        #     # NOTE: not terminate the worker, make it wait for processing further backward jobs
+        #     time.sleep(100)
 
-        dist.barrier()
+        # dist.barrier()
 
     def _construct_first_package(self, microbatch_idx: int, input: torch.Tensor):
         """Construct the first forward package of a microbatch."""

tests/nn/pipeline_parallel_2/run_engine.py
@@ -63,8 +63,11 @@ def run_pipeline_engine(
     p_outputs = pipeline_engine.run(inputs)
 
     if is_last_stage(parallel_context):
-        assert torch.allclose(p_outputs, outputs)
+        assert isinstance(p_outputs, list)
         assert forward_timeline == EXPECTED_FORWARD_TIMELINE
+
+        p_outputs = torch.cat(p_outputs, dim=0)
+        assert torch.allclose(p_outputs, outputs)
 
         from pipegoose.nn.pipeline_parallel2.queue import (
             _SAVED_ACTIVATIONS,
@@ -75,7 +78,7 @@ def run_pipeline_engine(
         saved_output = _SAVED_ACTIVATIONS[key]
 
         print(f"do backward from run_engine.py")
-        saved_output.sum().backward(retain_graph=True)
+        saved_output.sum().backward()
 
         import time
 