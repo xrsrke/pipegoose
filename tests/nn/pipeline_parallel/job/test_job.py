c90723333f82a66259e0878e564d844b71b79f2f
xrsrke <b3f0cus@icloud.com>
2023-10-20 11:53:32 +0700

    add testing the updated weight of PipelineParallel after .step()

--- tests/nn/pipeline_parallel/job/test_job.py
+++
@@ -1,1 +0,0 @@
-from pipegoose.nn.pipeline_parallel._job.job import Job
--- tests/nn/pipeline_parallel/test_pipeline_parallel.py
+++ tests/nn/pipeline_parallel/test_pipeline_parallel.py
@@ -3,6 +3,7 @@ from functools import reduce
 
 import torch
 from torch import nn
+from torch.optim import SGD
 
 from pipegoose.nn.pipeline_parallel._utils import get_partition_idx, is_last_stage
 from pipegoose.nn.pipeline_parallel.pipeline_parallel import PipelineParallel
@@ -22,18 +23,15 @@ def count_parameters(model):
 
 
 def run_pipeline_parallel(
-    rank,
-    world_size,
-    port,
-    tensor_parallel_size,
-    pipeline_parallel_size,
-    data_parallel_size,
-    num_microbatches,
-    model,
-    inputs,
-    ref_outputs,
-    ref_grads,
+    rank, world_size, port, tensor_parallel_size, pipeline_parallel_size, data_parallel_size, num_microbatches, kwargs
 ):
+    MODEL = kwargs["model"]
+    UPDATED_MODEL = kwargs["updated_model"]
+    INPUTS = kwargs["inputs"]
+    REF_OUTPUTS = kwargs["ref_outputs"]
+    REF_GRADS = kwargs["ref_grads"]
+    LR = kwargs["lr"]
+
     forward_timeline = []
     backward_timeline = []
 
@@ -55,8 +53,8 @@ def run_pipeline_parallel(
             self.module.microbatch_idx += 1
             return self.module(input)
 
-    # NOTE: just record the forward and backward timeline
-    model = nn.ModuleList([TimelineRegister(partition_idx, module) for partition_idx, module in enumerate(model)])
+    # NOTE: just for recording the forward and backward timeline
+    model = nn.ModuleList([TimelineRegister(partition_idx, module) for partition_idx, module in enumerate(MODEL)])
 
     parallel_context = init_parallel_context(
         rank, world_size, port, tensor_parallel_size, pipeline_parallel_size, data_parallel_size
@@ -64,47 +62,62 @@ def run_pipeline_parallel(
     partition_idx = get_partition_idx(parallel_context)
     EXPECTED_FORWARD_TIMELINE, EXPECTED_BACKWARD_TIMELINE = generate_expected_timeline(num_microbatches, partition_idx)
 
-    parallelized_model = PipelineParallel(
-        model, num_microbatches=num_microbatches, parallel_context=parallel_context
-    ).parallelize()
+    parallelized_model = PipelineParallel(model, num_microbatches, parallel_context).parallelize()
+    optim = SGD(parallelized_model.parameters(), LR)
 
     assert isinstance(parallelized_model, nn.Module)
     assert count_parameters(parallelized_model) < count_parameters(model)
     assert count_parameters(parallelized_model) == count_parameters(model[partition_idx])
 
-    outputs = parallelized_model(inputs)
+    outputs = parallelized_model(INPUTS)
 
     assert forward_timeline == EXPECTED_FORWARD_TIMELINE
     if is_last_stage(parallel_context):
-        assert torch.allclose(torch.cat(outputs, dim=0), ref_outputs)
+        assert torch.allclose(torch.cat(outputs, dim=0), REF_OUTPUTS)
 
+    optim.zero_grad()
     for output in outputs:
         output.sum().backward(retain_graph=True)
 
+    optim.step()
+
     assert backward_timeline == EXPECTED_BACKWARD_TIMELINE
-    for p, ref_grad in zip(parallelized_model.parameters(), ref_grads[partition_idx]):
-        assert p.grad is not None
+    for p, ref_grad in zip(parallelized_model.parameters(), REF_GRADS[partition_idx]):
         assert torch.allclose(p.grad, ref_grad)
+
+    for p, ref_p in zip(parallelized_model.parameters(), UPDATED_MODEL[partition_idx].parameters()):
+        assert torch.allclose(p, ref_p)
 
 
 def test_pipeline_parallel():
-    TENSOR_PARALLEL_SIZE = 1
-    PIPELINE_PARALLEL_SIZE = 4
-    DATA_PARALLEL_SIZE = 1
-
-    BATCH_SIZE = 32
-    NUM_MICROBATCHES = 6
-    SEQ_LEN = 10
-    HIDDEN_DIM = 5
+    TENSOR_PARALLEL_SIZE, PIPELINE_PARALLEL_SIZE, DATA_PARALLEL_SIZE = 1, 4, 1
     WORLD_SIZE = TENSOR_PARALLEL_SIZE * PIPELINE_PARALLEL_SIZE * DATA_PARALLEL_SIZE
+
+    BATCH_SIZE, NUM_MICROBATCHES = 32, 6
+    SEQ_LEN, HIDDEN_DIM = 10, 5
+    LR = 0.1
 
     inputs = torch.randn(BATCH_SIZE, SEQ_LEN, HIDDEN_DIM, requires_grad=False)
     model = nn.ModuleList([nn.Sequential(nn.Linear(HIDDEN_DIM, HIDDEN_DIM), nn.ReLU()) for _ in range(PIPELINE_PARALLEL_SIZE)])
     ORIG_MODEL = deepcopy(model)
+    optim = SGD(model.parameters(), lr=LR)
 
     outputs = reduce(lambda inputs, layer: layer(inputs), model, inputs)
+
+    optim.zero_grad()
     outputs.sum().backward()
+    optim.step()
+
     grads = [[p.grad for p in layer.parameters()] for layer in model]
+
+    kwargs = {
+        "lr": LR,
+        "model": ORIG_MODEL,
+        "updated_model": model,
+        "inputs": inputs.detach(),
+        "ref_outputs": outputs.detach(),
+        "ref_grads": grads,
+    }
 
     spawn(
         run_pipeline_parallel,
@@ -113,8 +126,5 @@ def test_pipeline_parallel():
         pipeline_parallel_size=PIPELINE_PARALLEL_SIZE,
         data_parallel_size=DATA_PARALLEL_SIZE,
         num_microbatches=NUM_MICROBATCHES,
-        model=ORIG_MODEL,
-        inputs=inputs.detach(),
-        ref_outputs=outputs.detach(),
-        ref_grads=grads,
+        kwargs=kwargs,
     )
