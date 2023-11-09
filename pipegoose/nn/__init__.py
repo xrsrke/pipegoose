46aedb7e4b86b074ce040d237a45bc87889ba1d9
xrsrke <b3f0cus@icloud.com>
2023-11-09 14:30:06 +0700

    [FEATURE] Add expert parallel module paths to nn module path

--- pipegoose/nn/__init__.py
+++ pipegoose/nn/__init__.py
@@ -1,3 +1,4 @@
 from pipegoose.nn.data_parallel.data_parallel import DataParallel
 from pipegoose.nn.tensor_parallel.tensor_parallel import TensorParallel
 from pipegoose.nn.pipeline_parallel.pipeline_parallel import PipelineParallel
+from pipegoose.nn.expert_parallel.expert_parallel import ExpertParallel
