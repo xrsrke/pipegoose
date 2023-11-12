fcd97bc30fab573fbbdbe06b25097dfdd666719e
Daniel Grittner <danielgrittner@web.de>
2023-11-12 12:16:25 +0100

    [FEATURE] add top-1 and top-2 routers

--- pipegoose/nn/expert_parallel/routers.py
+++ pipegoose/nn/expert_parallel/routers.py
@@ -1,24 +1,162 @@
-"""DON'T USE THIS MODULE: Under development."""
 from abc import ABC
+from typing import Tuple
+import math
 
+import torch
 from torch import nn
+import torch.nn.functional as F
+from torchtyping import TensorType
 
 
-class Router(ABC):
+class Router(ABC, nn.Module):
     pass
 
 
-class Top1Router(Router, nn.Module):
-    def __init__(self):
+class _TopKRouter(Router):
+    def __init__(
+        self,
+        top_k: int,
+        num_experts: int,
+        d_model: int,
+        expert_capacity: Tuple[float, float] = (1.0, 1.0),
+        alpha: float = 0.01,
+        eps: float = 0.1,
+        aux_loss_weight: float = 1.0,
+        z_loss_weight: float = 1.0
+    ):
         super().__init__()
+        self.top_k = top_k
+        self.num_experts = num_experts
+        self.expert_capacity = expert_capacity
+        self.alpha = alpha
+        self.eps = eps
+        self.aux_loss_weight = aux_loss_weight
+        self.z_loss_weight = z_loss_weight
+        self.gate = nn.Linear(d_model, num_experts)
 
-    def forward(self):
-        pass
+    def _aux_loss(
+        self,
+        router_prob: TensorType["batch_size*seq_len", "num_experts"],
+        expert_mask: TensorType["batch_size*seq_len", "num_experts"]
+    ) -> TensorType["1"]:
+        """
+        Auxiliary Load Balancing Loss as described in
+        Switch Transformers: Scaling to Trillion Parameter Models with Simple and Efficient Sparsity
+        by Fedus et al.
+        """
+        fraction_of_tokens_per_expert = expert_mask.mean(dim=0)
+        fraction_of_prob_mass_per_expert = router_prob.mean(dim=0)
+
+        inner_prod = fraction_of_tokens_per_expert @ fraction_of_prob_mass_per_expert
+        aux_loss = self.alpha * self.num_experts * inner_prod
+
+        return aux_loss
+
+    def _z_loss(
+        self,
+        router_logits: TensorType["batch_size*seq_len", "num_experts"]
+    ) -> TensorType["1"]:
+        """
+        z-loss as described in 
+        ST-MoE: Designing Stable and Transferable Sparse Expert Models
+        by Zoph et al.
+        """
+        return (torch.log(router_logits.exp().sum(dim=-1))**2).mean()
+
+    def _expert_capacity(self, total_tokens: int) -> int:
+        c = self.expert_capacity[0 if self.training else 1]
+        expert_capacity = math.ceil((total_tokens / self.num_experts) * c)
+        return expert_capacity
+
+    def forward(
+        self,
+        inputs: TensorType["batch_size", "seq_len", "d_model"]
+    ) -> Tuple[
+        TensorType["batch_size*seq_len", "num_experts"],
+        TensorType["batch_size*seq_len", "num_experts"],
+        TensorType["1"]
+    ]:
+        orig_dtype = inputs.dtype
+        total_tokens = inputs.shape[0] * inputs.shape[1]
+
+        # calculate router probability mass for each token
+        router_logits = self.gate(inputs.float()).reshape(-1, self.num_experts)
+        if self.training:
+            # exploration with noise sampled from [1-eps, 1+eps] during training
+            noise = torch.rand_like(router_logits) # between [0, 1)
+            noise = noise * self.eps * 2 # between [0, 2*eps)
+            noise += 1 - self.eps # between [1-eps, 1+eps)
+            router_logits += noise
+
+        router_prob = F.softmax(router_logits, dim=-1)
+
+        # for each token, select the top-k experts to which we send the token
+        topk_idxs = torch.topk(router_prob, self.top_k, dim=-1).indices
+
+        # compute expert mask, set True at position (i,j) if token i is routed to expert j
+        expert_mask = torch.zeros_like(router_prob)
+        expert_mask = expert_mask.scatter_(1, topk_idxs, True)
+
+        # calculate router loss
+        loss = self.aux_loss_weight * self._aux_loss(router_prob, expert_mask) + \
+            self.z_loss_weight * self._z_loss(router_logits)
+
+        # limit the number of tokens per expert
+        position_in_expert = torch.cumsum(expert_mask, dim=0) * expert_mask
+
+        # filter out tokens which exceed the capacity
+        expert_capacity = self._expert_capacity(total_tokens)
+        topk_expert_mask = expert_mask * (position_in_expert < expert_capacity)
+
+        # prune the router probabilities, only keep probabilities at position (i,j)
+        # if token i is routed to expert j
+        topk_weight = router_prob * topk_expert_mask
+        topk_weight = topk_weight.to(orig_dtype)
+
+        return topk_expert_mask, topk_weight, loss
 
 
-class Top2Router(Router, nn.Module):
-    def __init__(self):
-        super().__init__()
+class Top1Router(_TopKRouter):
+    def __init__(
+        self,
+        num_experts: int,
+        d_model: int,
+        expert_capacity: Tuple[float, float] = (1.0, 1.0),
+        alpha: float = 0.01,
+        eps: float = 0.1,
+        aux_loss_weight: float = 1.0,
+        z_loss_weight: float = 1.0
+    ):
+        super().__init__(
+            top_k=1,
+            num_experts=num_experts,
+            d_model=d_model,
+            expert_capacity=expert_capacity,
+            alpha=alpha,
+            eps=eps,
+            aux_loss_weight=aux_loss_weight,
+            z_loss_weight=z_loss_weight
+        )
 
-    def forward(self):
-        pass
+
+class Top2Router(_TopKRouter):
+    def __init__(
+        self,
+        num_experts: int,
+        d_model: int,
+        expert_capacity: Tuple[float, float] = (1.0, 1.0),
+        alpha: float = 0.01,
+        eps: float = 0.1,
+        aux_loss_weight: float = 1.0,
+        z_loss_weight: float = 1.0
+    ):
+        super().__init__(
+            top_k=2,
+            num_experts=num_experts,
+            d_model=d_model,
+            expert_capacity=expert_capacity,
+            alpha=alpha,
+            eps=eps,
+            aux_loss_weight=aux_loss_weight,
+            z_loss_weight=z_loss_weight
+        )
--- tests/nn/expert_parallel/test_routers.py
+++ tests/nn/expert_parallel/test_routers.py
@@ -1,8 +1,60 @@
 import torch
 
+from pipegoose.nn.expert_parallel import Top1Router, Top2Router
+
+
+def run_topk_router(
+    router,
+    batch_size,
+    seq_len,
+    d_model,
+    num_experts,
+    top_k
+):
+    input = torch.randn(batch_size, seq_len, d_model)
+    dispatch_order, gate_values, loss = router(input)
+
+    assert dispatch_order.shape == (batch_size*seq_len, num_experts)
+    assert gate_values.shape == (batch_size*seq_len, num_experts)
+    assert loss.shape == ()
+
+    total_tokens = batch_size * seq_len
+    expert_capacity = router._expert_capacity(total_tokens)
+
+    for expert_id in range(num_experts):
+        assert dispatch_order[..., expert_id].sum().item() < expert_capacity
+
+    for token_id in range(total_tokens):
+        assert dispatch_order[token_id, ...].sum().item() <= top_k
+
 
 def test_top1_router():
+    NUM_EXPERTS = 5
     BATCH_SIZE, SEQ_LEN, D_MODEL = 5, 10, 64
-    torch.randn(BATCH_SIZE, SEQ_LEN, D_MODEL)
 
-    # NOTE: dispatching_order.shape = [batch_size*seq_ken]
+    top1_router = Top1Router(NUM_EXPERTS, D_MODEL)
+
+    run_topk_router(
+        top1_router,
+        BATCH_SIZE,
+        SEQ_LEN,
+        D_MODEL,
+        NUM_EXPERTS,
+        top_k=1
+    )
+
+
+def test_top2_router():
+    NUM_EXPERTS = 5
+    BATCH_SIZE, SEQ_LEN, D_MODEL = 5, 10, 64
+
+    top2_router = Top2Router(NUM_EXPERTS, D_MODEL)
+
+    run_topk_router(
+        top2_router,
+        BATCH_SIZE,
+        SEQ_LEN,
+        D_MODEL,
+        NUM_EXPERTS,
+        top_k=2
+    )
