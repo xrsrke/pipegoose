f36b085e6f258091262f1bced2819ba80fa3f7d9
Daniel Grittner <danielgrittner@web.de>
2023-11-19 17:02:40 +0100

    Working partitioning implementation

--- pipegoose/nn/pipeline_parallel/partitioner.py
+++ pipegoose/nn/pipeline_parallel/partitioner.py
@@ -4,11 +4,7 @@ from typing import List
 from torch import nn
 import torch
 from typing import Dict
-from torch.fx.node import Node
-from typing import Set
-
-from transformers.models.auto import get_values
-from packaging import version
+from collections import defaultdict
 
 from pipegoose.distributed.parallel_context import ParallelContext
 from pipegoose.distributed.parallel_mode import ParallelMode
@@ -28,17 +24,15 @@ class BasePartitioner(ABC):
 
 
 class UniformPartitioner(BasePartitioner):
-    # def __init__(self, module: nn.Module, parallel_context: ParallelContext):
-    def __init__(self, module: nn.Module, n_partitions: int):
-        self.module = module
-        self.n_partitions = n_partitions
+    def __init__(self, model: nn.Module, parallel_context: ParallelContext):
+        self.model = model
+        self.parallel_context = parallel_context
 
     def _split_nodes(
         self, traced_graph_module: torch.fx.GraphModule, shard_count: int = 3
     ) -> Dict:
         """Utility used to trace a graph and identify shard cutpoints."""
 
-        nodes_so_far = []
         param_count: Dict[str, int] = {}
 
         # Find the total number of params in the model and
@@ -55,13 +49,11 @@ class UniformPartitioner(BasePartitioner):
         # in the traced graph while in the module we only see the projection as a whole module.
         
         for name, param in traced_graph_module.named_parameters():
-            print(f"{name} => {param.numel()}")
             name = name.replace(".", "_")
             param_count[name] = param.numel()
         
         total_param_count = 0
         for name, module in traced_graph_module.named_modules():
-            print(name)
             if len(name) > 0 and name.count(".") == 0:
                 # also note that the parameters of the lm_head for some models (e.g. GPT2) are not
                 # considered in named_parameters(). therefore, we must count the parameters using
@@ -78,10 +70,12 @@ class UniformPartitioner(BasePartitioner):
         per_shard_param = total_param_count // shard_count
         print(f"Per shard param count {per_shard_param}")
 
-
         node_name_to_shard_id: Dict[str, int] = {}
         shard_id = 0
         shard_id_to_param_count = [0 for _ in range(shard_count)]
+
+        output_from_shard = {}
+
         for node in traced_graph_module.graph.nodes:
             if node.op == "output":
                 break
@@ -100,54 +94,69 @@ class UniformPartitioner(BasePartitioner):
 
                 shard_id_to_param_count[shard_id] += current_param_count
                 print(f"shard_id_to_param_count = {shard_id_to_param_count}")
-            
+
+            # we need to collect the nodes from the previous shard which are needed in the
+            # current shard because the previous shard needs to output them
+            if hasattr(node, "args"):
+                for arg in node.args:
+                    if not hasattr(arg, "name"): 
+                        continue
+
+                    # the input to the current node is from the previous shard. remember it!
+                    arg_shard_id = node_name_to_shard_id.get(arg.name, shard_id)
+                    if arg_shard_id < shard_id:
+                        # propagate the input from arg_shard_id until shard_id
+                        for idx in range(arg_shard_id, shard_id):
+                            # note that we use the dict as an ordered set data structure
+                            output_from_shard.setdefault(idx, dict())[arg.name] = None
+
             node_name_to_shard_id[node.name] = shard_id
 
-        return node_name_to_shard_id
+        return node_name_to_shard_id, output_from_shard
 
-    def split(self, input_names) -> List[nn.Module]:
-        # n_partitions = self.parallel_context.pipeline_parallel_size
-        n_partitions = self.n_partitions # FIXME:
-        model = self.module
-        leaf_modules = set()
+    def split(self, input_names: List[str]) -> List[nn.Module]:
+        n_partitions = self.parallel_context.pipeline_parallel_size
+        model = self.model
         module_list: List[torch.fx.GraphModule] = []
         num_graphs = 0
         new_graph = torch.fx.Graph()  # type: ignore
-        env: Dict[str, Node] = {}
-        new_input_node = None
 
-        symbolic_traced_module = symbolic_trace(self.module)
+        symbolic_traced_module = symbolic_trace(model, input_names=input_names)
 
-        prev_shard_id = 1000
-        prev_shard_node = None
+        node_name_to_shard_id, output_from_shard = self._split_nodes(symbolic_traced_module, n_partitions)
 
-        node_name_to_shard_id = self._split_nodes(symbolic_traced_module, n_partitions)
+        nodes_per_shard = defaultdict(dict)
 
         prev_shard_id = 1000
         prev_node = None
         for node in symbolic_traced_module.graph.nodes:
             # If the current node is in the next shard, we insert an output node.
             # A new graph is created and a placeholder is added for the next shard.
-            if (
-                node.name in node_name_to_shard_id
-                and prev_shard_id < node_name_to_shard_id[node.name]
-            ):
+            if node.name in node_name_to_shard_id and prev_shard_id < node_name_to_shard_id[node.name]:
                 assert prev_node, "prev_node cannot be None"
 
+                # generate output node for the past graph/shard
                 with new_graph.inserting_after(prev_node):
-                    new_graph.output(env[prev_node.name])
+                    outputs = output_from_shard[prev_shard_id]
+                    graph_output_names = [prev_node.name] + [i for i in outputs if i != prev_node.name]
+
+                    if isinstance(nodes_per_shard[prev_shard_id][prev_node.name], tuple):
+                        graph_outputs = nodes_per_shard[prev_shard_id][prev_node.name] + \
+                            tuple([nodes_per_shard[prev_shard_id][i] for i in outputs if i != prev_node.name])
+                    else:
+                        graph_outputs = tuple([nodes_per_shard[prev_shard_id][prev_node.name]] + \
+                                              [nodes_per_shard[prev_shard_id][i] for i in outputs if i != prev_node.name])
+                    new_graph.create_node(op="output", target="output", args=(graph_outputs,))
+
+                # generate new graph/shard and its input nodes (i.e., the output from the previous graph/shard)
                 num_graphs += 1
                 module_list.append(torch.fx.GraphModule(model, new_graph))
                 new_graph = torch.fx.Graph()
-                node_name = "placeholder" + str(num_graphs)
-                pl_node = new_graph.create_node("placeholder", node_name)
-                env[node_name] = pl_node
-                new_input_node = pl_node
+                # generate placeholder nodes in the new graph/shard which matches the output nodes of the previous graph/shard
+                for new_graph_input_name in graph_output_names:
+                    graph_input_node = new_graph.create_node("placeholder", new_graph_input_name)
+                    nodes_per_shard[node_name_to_shard_id[node.name]][new_graph_input_name] = graph_input_node
 
-            if new_input_node is not None:
-                # Account for a placeholder in the new graph.
-                node.args = (new_input_node,)
-                new_input_node = None
             if node.op in [
                 "placeholder",
                 "get_attr",
@@ -156,15 +165,16 @@ class UniformPartitioner(BasePartitioner):
                 "call_module",
             ]:
                 # Copy the nodes from the existing graph to the new graph.
-                new_node = new_graph.node_copy(node, lambda x: env[x.name])
-                env[node.name] = new_node
+                current_shard_id = node_name_to_shard_id[node.name]
+                new_node = new_graph.node_copy(node, lambda x: nodes_per_shard[current_shard_id][x.name])
+                nodes_per_shard[current_shard_id][node.name] = new_node
             elif node.op == "output":
                 # If this is the last node, we should add an output
                 # node and add the last graph to the list.
                 assert prev_node, "prev_node cannot be None"
 
                 with new_graph.inserting_after(prev_node):
-                    new_graph.output(env[prev_node.name])
+                    new_graph.output(nodes_per_shard[prev_shard_id][prev_node.name])
                 module_list.append(torch.fx.GraphModule(model, new_graph))
                 break
             prev_node = new_node
--- tests/nn/pipeline_parallel/test_partitioner.py
+++ tests/nn/pipeline_parallel/test_partitioner.py
@@ -15,58 +15,37 @@ def get_small_gpt2_and_tokenizer(n_layer=12):
     ), AutoTokenizer.from_pretrained("gpt2")
 
 
-# def run_model_partitioner(
-#     rank,
-#     world_size,
-#     port,
-#     tensor_parallel_size,
-#     pipeline_parallel_size,
-#     data_parallel_size,
-# ):
-#     parallel_context = init_parallel_context(
-#         rank,
-#         world_size,
-#         port,
-#         tensor_parallel_size,
-#         pipeline_parallel_size,
-#         data_parallel_size,
-#     )
-#     model = get_small_gpt2()
-#     partitions = UniformPartitioner(model, parallel_context).split(["input_ids"])
+def run_model_partitioner(
+    rank,
+    world_size,
+    port,
+    tensor_parallel_size,
+    pipeline_parallel_size,
+    data_parallel_size,
+):
+    parallel_context = init_parallel_context(
+        rank,
+        world_size,
+        port,
+        tensor_parallel_size,
+        pipeline_parallel_size,
+        data_parallel_size,
+    )
 
-#     # partition = get_model_partition(module, policy, parallel_context)
-
-#     for p in partitions:
-#         print("==================")
-#         print(sum([x.numel() for x in p.parameters()]))
-#         print("==================")
-    
-#     assert False
-
-
-@pytest.mark.parametrize("pipeline_parallel_size", [4])
-def test_naive_partitioning(pipeline_parallel_size):
-    # TENSOR_PARALLEL_SIZE = 1
-    # DATA_PARALLEL_SIZE = 1
-
-    # spawn(
-    #     run_model_partitioner,
-    #     world_size=pipeline_parallel_size,
-    #     tensor_parallel_size=TENSOR_PARALLEL_SIZE,
-    #     pipeline_parallel_size=pipeline_parallel_size,
-    #     data_parallel_size=DATA_PARALLEL_SIZE,
-    # )
-
+    torch.manual_seed(0)
     batch_sentences = ["hello world from pipegoose"]
 
     model, tokenizer = get_small_gpt2_and_tokenizer()
+    model.eval()
     tokenizer.pad_token = tokenizer.eos_token
+    inputs = tokenizer(batch_sentences, padding=True, return_tensors="pt")
+    gt_logits = model(input_ids=inputs["input_ids"]).logits
 
-    partitioned_model = UniformPartitioner(model, pipeline_parallel_size).split(["input_ids"])
+    partitioned_model = UniformPartitioner(model, parallel_context).split(["input_ids"])
+    assert len(partitioned_model) == pipeline_parallel_size
 
     for p in partitioned_model:
         print("==================")
-        # print(p)
         print(sum([x.numel() for x in p.parameters()]))
         print("==================")
 
@@ -74,10 +53,23 @@ def test_naive_partitioning(pipeline_parallel_size):
 
     partitioned_model_result = inputs["input_ids"]
     for partition_id in range(pipeline_parallel_size):
-        partitioned_model_result = partitioned_model[partition_id](partitioned_model_result)
+        if type(partitioned_model_result) in (list, tuple):
+            partitioned_model_result = partitioned_model[partition_id](*partitioned_model_result)
+        else:
+            partitioned_model_result = partitioned_model[partition_id](partitioned_model_result)
 
-    gt_result = model(**inputs)
+    assert torch.allclose(gt_logits, partitioned_model_result), "Results are not close"
 
-    assert torch.allclose(gt_result, partitioned_model_result)
 
-    assert False, "Debug"
+@pytest.mark.parametrize("pipeline_parallel_size", [2, 3, 4])
+def test_naive_partitioning(pipeline_parallel_size):
+    TENSOR_PARALLEL_SIZE = 1
+    DATA_PARALLEL_SIZE = 1
+
+    spawn(
+        run_model_partitioner,
+        world_size=pipeline_parallel_size,
+        tensor_parallel_size=TENSOR_PARALLEL_SIZE,
+        pipeline_parallel_size=pipeline_parallel_size,
+        data_parallel_size=DATA_PARALLEL_SIZE,
+    )
