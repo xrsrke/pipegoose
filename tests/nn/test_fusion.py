3397ba99211a457d464e16ff550de056e0fbea9a
Sami <samibghanem@gmail.com>
2023-11-20 01:19:26 -0500

    Tests failing

--- tests/nn/test_fusion.py
+++ tests/nn/test_fusion.py
@@ -6,23 +6,33 @@ from pipegoose.nn.fusion import FusedBiasDropout, FusedBiasGelu
 
 
 def test_FusedBiasDropout():
-    dropout = FusedBiasDropout()
-    input = torch.randn(20, 16)
-    args = (0.5, False)
-    expected = Dropout(*args)(input)
-    actual = dropout(input)
+    dropout_p, inplace, training = 0.5, False, True
 
+    fused_bias_dropout = FusedBiasDropout(
+        dropout_p=dropout_p, inplace=inplace
+    )
+
+    input = torch.randn(20, 16)
+    bias = torch.randn(16)
+    torch.manual_seed(0)
+
+    expected = Dropout(p=dropout_p, inplace=inplace)(input + bias)
+    actual = fused_bias_dropout(input, bias)
+    
     assert actual.size() == expected.size()
     assert torch.allclose(actual, expected)
-    assert FusedBiasDropout(*args).represents == Dropout
+    assert fused_bias_dropout.represents == Dropout
 
 
 def test_FusedBiasGelu():
-    gelu = FusedBiasGelu()
+    fused_bias_gelu = FusedBiasGelu()
     input = torch.randn(20, 16)
-    expected = GELU()(input)
-    actual = gelu(input)
+    bias = torch.randn(16)
+    torch.manual_seed(0)
+
+    expected = GELU()(input + bias)
+    actual = fused_bias_gelu(input, bias)
 
     assert actual.size() == expected.size()
     assert torch.allclose(actual, expected)
-    assert FusedBiasGelu().represents == GELU
+    assert fused_bias_gelu.represents == GELU
